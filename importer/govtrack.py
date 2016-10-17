f98ef7a6cba5b5507a75825b4c335000dcf519b5
yysgorg <yysgorg@gmail.com>
2016-10-17 19:44:40 +0200

    - collecting dates of votings and birthdays of voters

--- importer/govtrack.py
+++ importer/govtrack.py
@@ -1,6 +1,7 @@
 
 import urllib2
 
+import time
 from flask import json
 
 TEST = True
@@ -8,7 +9,7 @@ TEST = True
 # number of bills to query per bulk
 BULK_SIZE = 100
 if TEST:
-    BULK_SIZE = 3
+    BULK_SIZE = 5
 
 def constructData():
     
@@ -38,7 +39,7 @@ def constructData():
         return get_data("https://www.govtrack.us/api/v2/vote_voter?vote=" + str(vote_event['id']))['objects']
 
     def parseVoteEvent(vote_event):
-        return '<https://www.govtrack.us/api/v2/vote?id=' + str(vote_event['id'])+">"
+        return '<https://www.govtrack.us/api/v2/vote/' + str(vote_event['id'])+">"
 
     def cleanString(str):
         return str.replace('"', '').replace('\'', '')
@@ -57,8 +58,10 @@ def constructData():
         return {'+': 'v:votesYay','-': 'v:votesNay','0': 'v:abstains'}[o['key']]
 
     def voterVotesTriple(vote,vote_event):
-        return [parseVoter(vote['person']['id']), parseDirection(vote['option']), parseVoteEvent(vote_event)]
-
+        return [
+            parseVoter(vote['person']['id']),
+                parseDirection(vote['option']),
+                parseVoteEvent(vote_event)]
 
 
     num_voteEvents = get_numberOfVoteEvents()
@@ -67,39 +70,68 @@ def constructData():
 
 
     bill_text_triples = []
+    vote_date_triples = []
     voter_vote_triples = []
+    
+    birthday_dict = {}
     party_membership_dict = {}
+    
+
+    # instead of voter age, we should get birthdays, and deduce age for each VotingEvent
+    #   (as age is different for different VotingEvents).
+    # because this is more complicated, we leave it out for now and do this instead,
+    #   in order to meet the assignment requirements.
+    voter_age_dict = {}
 
     offset = 0
+    def parseDate(date):
+        return "\"" + date[:10] + "\"^^xsd:date"
 
+    def parseVoteDate(voting_data):
+        return [parseVoteEvent(voting_data),'rdfs:date',parseDate(voting_data['created'])]
+
+    start_time = time.time()
+    times = []
+    i = 0
     while (offset < num_voteEvents):
 
         vote_events = get_voteEventBulk()
 
         for vote_event in vote_events:
-            print vote_event['id']
+            print "Vote Event" , vote_event['id']
             # add the bill text
             bill_text_triples.append(billTextTriple(vote_event))
 
-            # get the individual votes
-            voting_data = get_votingData(vote_event)
+            # get the individual votesr
+            votes = get_votingData(vote_event)
+            vote_date_triples.append(parseVoteDate(vote_event))
 
-            for vote in voting_data:
-                if vote['person']['id'] not in party_membership_dict:
-                    party_membership_dict[vote['person']['id']] = get_partyMembership(vote['person']['id'])
-                    print party_membership_dict[vote['person']['id']]
-                print 'vote ',vote['id']
+            for vote in votes:
+                print "Vote #",vote['id']
+                voter_id = vote['person']['id']
+                if voter_id not in birthday_dict:
+                    birthday_dict[voter_id] = vote['person']['birthday']
+                if voter_id not in party_membership_dict:
+                    party_membership_dict[voter_id] = get_partyMembership(voter_id)
                 voter_vote_triples.append(voterVotesTriple(vote, vote_event))
-            break
+                if TEST:
+                    i+=1
+                    if i>5:break
+
+            times.append(time.time() - start_time)
+
         offset += BULK_SIZE
 
         # to be nice to the data provider during testing
         if TEST:
             break
 
-    party_membership_triples = []
-    for k in party_membership_dict:
-        if party_membership_dict[k] is not None:
-            party_membership_triples.append([parseVoter(k),'v:memberOf','"'+party_membership_dict[k]+'"'])
-    print party_membership_triples
-    return bill_text_triples + voter_vote_triples + party_membership_triples
+    party_membership_triples = [[parseVoter(k),'v:memberOf','"'+party_membership_dict[k]+'"'] for k in party_membership_dict if party_membership_dict[k] is not None]
+    print birthday_dict
+    #birthday_triples = [dateTriple(parseVoter(k),'foaf:birthday',birthday_dict[k]) for k in birthday_dict]
+    birthday_triples = []
+    for i in birthday_dict: birthday_triples.append([parseVoter(k),'dbo:birthDate',parseDate(birthday_dict[k])])
+
+    print 'Times per vote event:',times
+
+    return [['@prefix','dbo:','<http://dbpedia.org/ontology/>'],['@prefix','rdfs:','<http://www.w3.org/2000/01/rdf-schema#>']] + bill_text_triples + voter_vote_triples + party_membership_triples + vote_date_triples + birthday_triples
