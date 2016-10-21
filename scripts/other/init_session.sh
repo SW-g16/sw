bdf3dcbb44f2d5baaeac51c0164afa213bf19d37
yysgorg <yysgorg@gmail.com>
2016-10-21 22:39:24 +0200

    - refactoring (db_puter) - removed redundant nodejs download

--- scripts/other/init_session.sh
+++ scripts/other/init_session.sh
@@ -2,6 +2,6 @@
 
 guake -n guake -e 'cd ~/sw/scripts/other && sh ./stardog.sh' guake -r 'Stardog'
 
-guake -n guake -e 'sh ~/sw/scripts/other/app.sh' guake -r 'App'
+guake -n guake -e 'sh ~/sw/scripts/other/db_puter.sh' guake -r 'App'
 
 guake -n guake -e 'cd ~/sw/ld-r && npm run dev &' guake -r 'LD-R'
--- scripts/other/install/install.sh
+++ scripts/other/install/install.sh
@@ -15,10 +15,6 @@ sudo apt-get -y install python-software-properties python g++ make
 sudo apt-get -y install nodejs
 sudo apt-get -y install npm
 
-wget https://nodejs.org/dist/v6.7.0/node-v6.7.0-linux-x64.tar.xz
-tar xf node-v6.7.0-linux-x64.tar.xz
-rm node-v6.7.0-linux-x64.tar.xz
-
 sudo pip install flask
 sudo pip install SPARQLWRAPPER
 
