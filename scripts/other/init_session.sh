7842df69f22578c8de8428a63bb614c2dae5ffa0
yysgorg <yysgorg@gmail.com>
2016-10-21 15:38:24 +0200

    - revised to open the diff software in new guake tabs. get guake! it's a neat terminal

--- scripts/other/init_session.sh
+++ scripts/other/init_session.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd ~/sw/scripts/other
-sh ./stardog.sh 
-sh ./reset-db.sh 
-sh ./app.sh &
-cd ../../ld-r
-npm run dev &
+
+guake -n guake -e 'cd ~/sw/scripts/other && sh ./stardog.sh' guake -r 'Stardog'
+
+guake -n guake -e 'sh ~/sw/scripts/other/app.sh' guake -r 'App'
+
+guake -n guake -e 'cd ~/sw/ld-r && npm run dev &' guake -r 'LD-R'
