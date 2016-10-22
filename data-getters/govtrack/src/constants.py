0e31ce71958c4d78c60316c5dc4af9528d311483
Chadsr <ross@rchadwick.co.uk>
2016-10-22 22:25:35 +0200

    Fixed pathing inconsistencies

--- data-getters/govtrack/src/constants.py
+++ data-getters/govtrack/src/constants.py
@@ -1,7 +1,14 @@
+import os
+
 MAX_ACTIVE_THREADS = 8
-CONGRESS_PATH = 'govtrack-data/data/congress/'  # the location of the downloaded bulk data, relative from where you call this script
+PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))) # the location of the downloaded bulk data
+DATA_PATH = PATH+'/data'
+CONGRESS_PATH = DATA_PATH+'/govtrack/congress/'
 VOTE_PROPERTIES = [':votesYay', ':votesNay', ':abstains']
 VOTE_KEYS = ['Yea', 'Nay', 'Not Voting']
 EMPTY_THRESHOLD = 1500
 
 BASE_URI = 'http://votes.example.com/ontology/'
+
+print PATH
+print DATA_PATH
