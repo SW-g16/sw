390ccda4c0e719d614bb3e17053c9c51d40871bd
yysgorg <yysgorg@gmail.com>
2016-10-23 22:00:46 +0200

    - mining voter data from govtrack csv  - separated send_to_db function to new file, because it's now used on >1 file

--- data-getters/govtrack/src/session_processor.py
+++ data-getters/govtrack/src/session_processor.py
@@ -1,10 +1,8 @@
-from pprint import pprint
 
-import requests
-import time
 
 import bill_processor, fsinterface, constants as c
 
+import send_to_db
 
 # this is where most of the adaption to the endpoint happens.
 # this function processes a 'session' - a piece of the totality of congress'
@@ -12,21 +10,20 @@ import bill_processor, fsinterface, constants as c
 # there is currently a total of 114 historic sessions,
 #  and this function processes all the bills and votes for one of them.
 
-def send_to_db(triples):
-    rdf = '.\n'.join(["%s %s %s" % (t[0],t[1],t[2]) for t in triples if len(t)==3]) + '.'
-    requests.post('http://localhost:5000/store', data={'data': rdf})
 
 def process_session(session_id,threshold=1500):
     # we declare an array of triples which we will fill while traversing the data directory
+
+
 
     def depth_1_folders(): return fsinterface.get_int_dirnames('%s%d/votes' % (c.CONGRESS_PATH, session_id))
 
     def depth_2_folders(g): return fsinterface.get_dirnames('%s%d/votes/%d' % (c.CONGRESS_PATH, session_id, g))
 
     triples = []
-    t_0=0
+
     for g in depth_1_folders():
         for b in depth_2_folders(g):
             triples += bill_processor.process_bill(session_id, g, b)
-    send_to_db(triples)
+    send_to_db.send_to_db(triples)
     return len(triples)
--- data-getters/govtrack/src/voter_process.py
+++ data-getters/govtrack/src/voter_process.py
@@ -27,7 +27,8 @@
 
 0   foaf:firstname
 1   foaf:lastname
-2   foaf:gender
+// 2 // todo birthday
+3   foaf:gender
 4   :votesIn dbr:United_States_House_of_Representatives | :votesIn dbr:United_States_Senate
 5   :respresentsState dbr:Mississippi // however we get only the acronym. we use the map ross linked to to get the whole name
 7   :memberOf dbr:Republican_Party_(United_States)
@@ -36,22 +37,75 @@
 28  :wikipedia <http://www.wikipedia.org/wiki/wikipedia_id>
 
 """
+from pprint import pprint
+
+import send_to_db
 import os
 
 path = os.path.dirname(os.path.realpath(__file__))+'/../../../data/govtrack/congress-legislators/legislators-current.csv'
 
 import csv
 
+states = {'AK': 'Alaska','AL': 'Alabama','AR': 'Arkansas','AS': 'American Samoa','AZ': 'Arizona','CA': 'California','CO': 'Colorado','CT': 'Connecticut','DC': 'District of Columbia','DE': 'Delaware','FL': 'Florida','GA': 'Georgia','GU': 'Guam','HI': 'Hawaii','IA': 'Iowa','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','KS': 'Kansas','KY': 'Kentucky','LA': 'Louisiana','MA': 'Massachusetts','MD': 'Maryland','ME': 'Maine','MI': 'Michigan','MN': 'Minnesota','MO': 'Missouri','MP': 'Northern Mariana Islands','MS': 'Mississippi','MT': 'Montana','NA': 'National','NC': 'North Carolina','ND': 'North Dakota','NE': 'Nebraska','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','PR': 'Puerto Rico','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VA': 'Virginia','VI': 'Virgin Islands','VT': 'Vermont','WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'}
+
+interesting_indices = [0,1,2,4,5,7,18,23,28]
+
+def get_voter_uri(param):
+    return '<http://www.govtrack.us/api/v2/person/%s>'% param
+
+def get_party(key):
+    # todo parse properly and return dbpedia uri
+    return '"%s"'%key
+
 with open(path, 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     headers = []
     i=0
+
+    triples = [
+        ('@prefix',':','<http://www.votes.example.com/ontology/>'),
+        ('@prefix','dbr:','<http://dbpedia.org/resource/>'),
+        ('@prefix','owl:','<http://www.w3.org/2002/07/owl#>'),
+        ('@prefix','foaf:','<http://xmlns.com/foaf/0.1/>')
+    ]
+
     for row in spamreader:
         if i==0:
             headers = row
             i=1
-            print headers
+            pprint(headers)
             continue
-        print row
-        #
-        break
+
+        pprint(row)
+
+
+
+        # voter uri, using govtrack id.
+        # advantage: can be used to access govtrack voter obects,
+        #   like https://www.govtrack.us/api/v2/person/411931
+        voter_uri = get_voter_uri(row[23])
+
+        first_name_triple = (voter_uri, 'foaf:firstName', '"%s"'% row[0])
+
+        last_name_triple = (voter_uri,'foaf:lastName','"%s"'%row[1])
+
+        gender_triple = (voter_uri,'foaf:gender','"male"' if row[2]=='M' else '"female"')
+
+        votesIn_triple = (voter_uri,':votesIn','dbr:United_States_House_of_Representatives' if row[4] == 'rep' else 'dbr:United_States_Senate')
+
+        representsState_triple = (voter_uri,':representsState','dbr:'+states[row[5]].replace(' ','_'))
+
+        party_triple = (voter_uri,':memberOf' , "%s" % get_party(row[7]))
+
+        bioguide_id_triple = (voter_uri,'owl:sameAs','<gt_v/%s>'%row[18]) # associate with reviously stored voters
+
+        wikipedia_id_triple = (voter_uri,':wikipedia','<http://www.wikipedia.org/wiki/%s>'%row[28])
+
+        triples += [first_name_triple, last_name_triple, gender_triple,votesIn_triple,representsState_triple, party_triple
+#bioguide_id_triple,wikipedia_id_triple # todo integrate these as well
+]
+        if i==1:
+            break
+
+    print send_to_db.send_to_db(triples[:len(triples)])
+    print len(triples)
